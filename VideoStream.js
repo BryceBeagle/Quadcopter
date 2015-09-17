eb65eee8b011bdde89b00ae3d9b3ff846bd4c442
Bryce Beagle <bryce.beagle@gmail.com>
2015-09-17 00:54:09 -0700

    Started project

--- VideoStream.js
+++ VideoStream.js
@@ -1,34 +1,60 @@
-// Run this to save a h264 video file, with the PaVE frame filtered out.
-// You can then use this file as a ffmpeg source for additional processing
-// or streaming to a ffserver
+//
+//
+//
+// var arDrone = require('ar-drone')
+// var cv      = require('opencv')
+// var http    = require('http')
+// var output  = require('fs').createWriteStream('./vid.h264');
+//
+// console.log("Connecting video stream")
+//
+// var client = arDrone.createClient();
+// client.config('general:navdata_demo', 'FALSE');
+//
+// // var PaVEParser = require('ar-drone/lib/video/PaVEParser');
+//
+// //
+// var video = arDrone.createClient().getVideoStream();
+// // var parser = new PaVEParser();
+// //
+// // parser
+// //   .on('data', function(data) {
+// //     output.write(data.payload);
+// //   })
+// //   .on('end', function() {
+// //     output.end();
+// //   });
+// //
+// // video.pipe(parser);
+//
+// client.disableEmergency()
+//
+// console.log("no")
+//
+// client.takeoff(function() {
+//     console.log("k")
+//     client.land(function() {
+//         console.log("Tyler")
+//     })
+// })
+
+
+// Run this to receive a png image stream from your drone.
 
 var arDrone = require('ar-drone')
-var client  = arDrone.createClient();
-client.config('general:navdata_demo', 'FALSE');
+var cv      = require('opencv')
+var http    = require('http')
+var fs      = require('fs')
 
-// var PaVEParser = require('ar-drone/lib/video/PaVEParser');
-// var output = require('fs').createWriteStream('./vid.h264');
-//
-// var video = arDrone.createClient().getVideoStream();
-// var parser = new PaVEParser();
-//
-// parser
-//   .on('data', function(data) {
-//     output.write(data.payload);
-//   })
-//   .on('end', function() {
-//     output.end();
-//   });
-//
-// video.pipe(parser);
+var currentFrame
 
-client.disableEmergency()
+//var stream  = arDrone.createUdpNavdataStream();
+var client = arDrone.createClient()
+var videoStream = client.getVideoStream()
 
-console.log("no")
-
-client.takeoff(function() {
-    console.log("k")
-    client.land(function() {
-        console.log("Tyler")
+videoStream
+    .on('error', console.log)
+    .on('data', function(videoBuffer) {
+        currentFrame = videoBuffer
+        console.log(videoBuffer)
     })
-})
