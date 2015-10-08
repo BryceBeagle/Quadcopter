13494a1c3eedd840658d6be823b2c65fc453661d
Bryce Beagle <bryce.beagle@gmail.com>
2015-10-08 15:52:26 -0700

    Rudimentary Video Stream

--- VideoStream.js
+++ VideoStream.js
@@ -1,60 +1,54 @@
-//
-//
-//
-// var arDrone = require('ar-drone')
-// var cv      = require('opencv')
-// var http    = require('http')
-// var output  = require('fs').createWriteStream('./vid.h264');
-//
-// console.log("Connecting video stream")
-//
-// var client = arDrone.createClient();
-// client.config('general:navdata_demo', 'FALSE');
-//
-// // var PaVEParser = require('ar-drone/lib/video/PaVEParser');
-//
-// //
-// var video = arDrone.createClient().getVideoStream();
-// // var parser = new PaVEParser();
-// //
-// // parser
-// //   .on('data', function(data) {
-// //     output.write(data.payload);
-// //   })
-// //   .on('end', function() {
-// //     output.end();
-// //   });
-// //
-// // video.pipe(parser);
-//
-// client.disableEmergency()
-//
-// console.log("no")
-//
-// client.takeoff(function() {
-//     console.log("k")
-//     client.land(function() {
-//         console.log("Tyler")
-//     })
-// })
-
-
-// Run this to receive a png image stream from your drone.
-
 var arDrone = require('ar-drone')
 var cv      = require('opencv')
-var http    = require('http')
 var fs      = require('fs')
+var io      = require('socket.io').listen(5001)
+var dl      = require('delivery')
+var watch   = require('watch')
+var EventEm = require('events').EventEmitter
 
-var currentFrame
+console.log('Connecting png stream ...')
 
-//var stream  = arDrone.createUdpNavdataStream();
-var client = arDrone.createClient()
-var videoStream = client.getVideoStream()
+var client    = arDrone.createClient()
+var pngStream = client.getPngStream()
+var emitter   = new EventEm()
+var count = 1
 
-videoStream
+var currentFrame                            // Current frame of the video stream
+
+pngStream
     .on('error', console.log)
-    .on('data', function(videoBuffer) {
-        currentFrame = videoBuffer
-        console.log(videoBuffer)
+    .on('data', function(pngBuffer) {
+       currentFrame = pngBuffer
+       fs.writeFile('./currentFrame.png', currentFrame, function(error){
+           if (error) {
+               console.error("Error writing file: " + error.message)
+           } else {
+               console.log("File create successfully")
+               emitter.emit('frameUpdate')
+           }
+       })
+  })
+
+io.sockets.on('connection', function(socket) {
+    console.log("Connected")
+    var delivery = dl.listen(socket)
+    delivery.on('delivery.connect', function(delivery) {
+        connected = true
+        console.log("connected")
+
+        emitter.on('frameUpdate', function() {
+            console.log("Sending file")
+            delivery.send({
+                name: 'currentFrame.png',
+                path: './currentFrame.png'
+            })
+            console.log(count)
+            count++
+        })
+
+        delivery.on('send.success', function(file){
+            console.log("File successfully sent to client")
+        })
+
     })
+})
