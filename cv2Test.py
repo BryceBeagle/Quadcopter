3de70aa5f4949bfb654073974697455ee9ac41ab
Bryce Beagle <bryce.beagle@gmail.com>
2015-10-24 19:02:42 -0700

    Refactored code and deleted debug lines

--- cv2Test.py
+++ cv2Test.py
@@ -11,10 +11,8 @@ def main():
 
     global frame
     global frameMod
-    global currentState
-    currentState = 0
 
-    videoIP = 'tcp://192.168.1.1:5555'
+    frontVideoIP = 'tcp://192.168.1.1:5555'
 
     # Should the program still be running
     global running
@@ -27,7 +25,7 @@ def main():
     robotThread.start()
 
     # Get Video Stream from Quadcopter
-    videoStream = cv2.VideoCapture(videoIP)
+    videoStream = cv2.VideoCapture(frontVideoIP)
 
     while running:
         # Get current frame of Video Stream
@@ -40,7 +38,6 @@ def main():
 
         # Draw current frame to OpenCV window
         cv2.imshow('Video Stream', frameMod)
-        print currentState
 
         # Wait for escape character (Escape Key) to be pressed. Once pressed, OpenCV window is closed
         if cv2.waitKey(1) & 0xFF == 27:
@@ -67,26 +64,16 @@ def detectFaces():
         # Skip face detection until first frame of Video Stream is found
         if frame is None: continue
 
-        currentState = 1
-
         frameMod = frame.copy()
-
-        currentState = 2
 
         # Create grayscale version of video feed for OpenCV
         grayscale = cv2.cvtColor(frameMod, cv2.COLOR_BGR2GRAY)
 
-        currentState = 3
-
         # Create array of faces found using Haar Cascade face detection algorithm
         faces = faceCascade.detectMultiScale(grayscale, 1.3, 5)
 
-        currentState = 4
-
         for (x, y, w, h) in faces:
             cv2.rectangle(frameMod, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-        currentState = 5
 
 if __name__ == '__main__':
     main()
