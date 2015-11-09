795286b572b87bfd7f6b079a728b53085e3284d7
Bryce Beagle <bryce.beagle@gmail.com>
2015-11-08 19:28:40 -0700

    Used constants from ColorConstants.py

--- Tests/CircleDetection.py
+++ Tests/CircleDetection.py
@@ -1,13 +1,9 @@
 __author__ = 'Bryce Beagle'
 
-from OpenCVOps import ImageTransformLib, ImageSearchLib
 import cv2
 
-# TODO: Implement values in ImageTransformLib
-colorBoundaries = {"Red"   : ([350, 100, 100], [ 10, 255, 255]),
-                   "Green" : ([110, 100, 100], [130, 255, 255]),
-                   "Blue"  : ([230, 100, 100], [250, 255, 255])}
-
+from OpenCVOps import ImageTransformLib, ImageSearchLib
+from OpenCVOps.ColorConstants import Colors, ColorRanges
 
 convert   = ImageTransformLib.ImageTransform()
 searchFor = ImageSearchLib.ImageSearch()
@@ -16,20 +12,20 @@ def main():
 
     testImage = cv2.imread('TestLine.jpg')
 
-    isolatedTestImage = isolateColor(testImage, "Green")
+    isolatedTestImage = isolateColor(testImage, ColorRanges.GREEN_RANGE)
     circles = identifyFeatures(testImage, circles=True)[0]
 
     drawFeatures(testImage, circles)
 
 
-def isolateColor(testImage, color):
+def isolateColor(testImage, colorRange):
     """Attempts to find a line of the specified color in the supplied image."""
 
     # Create a new window to display output
     cv2.namedWindow("Isolated Color with Identified Features", cv2.WINDOW_NORMAL)
 
     # Isolate the specified color in the test image
-    testImageColor    = convert.RGBtoHSVRange(colorBoundaries[color])
+    testImageColor    = convert.RGBtoHSVRange(colorRange)
     isolatedTestImage = convert.toColorRange(testImage, testImageColor)
 
     return isolatedTestImage
@@ -55,8 +51,6 @@ def identifyFeatures(image, circles=None, triangles=None):
 
 def drawFeatures(image, circles=None, triangles=None):
 
-    print circles
-
     if circles is not None:
 
         print "Circles:\n", circles
@@ -66,9 +60,9 @@ def drawFeatures(image, circles=None, triangles=None):
             print "i:\n", i
 
             # Draw the circle
-            cv2.circle(image, (i[0], i[1]), i[2], (255, 0, 0), 2)
+            cv2.circle(image, (i[0], i[1]), i[2], Colors.RED, 2)
             # Draw the center point of the circle
-            cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
+            cv2.circle(image, (i[0], i[1]), 2, Colors.GREEN, 3)
 
     cv2.imshow("Isolated Color with Identified Features", image)
 
